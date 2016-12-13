8a795fd95f06fe8403cdd6e2b4c293976832dc46
starbass <sereda_sereda1@mail.ru>
2016-12-14 01:24:42 +0200

    ADD:

--- src/main/java/HW4/pages/CreatePlayerPage.java
+++ src/main/java/HW4/pages/CreatePlayerPage.java
@@ -3,72 +3,85 @@ package HW4.pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class CreatePlayerPage {
+    @FindBy(id = "ff14642ac1c__us_login")
+    private WebElement userNameInput;
+    @FindBy(id = "ff14642ac1c__us_email")
+    private WebElement emailInput;
+    @FindBy(id = "ff14642ac1c__us_password")
+    private WebElement passwInput;
+    @FindBy(id = "ff14642ac1c__confirm_password")
+    private WebElement confirmPasswordInput;
+    @FindBy(id = "ff14642ac1c__us_fname")
+    private WebElement firstNameInput;
+    @FindBy(id = "ff14642ac1c__us_lname")
+    private WebElement lastNameInput;
+    @FindBy(id = "ff14642ac1c__us_city")
+    private WebElement cityInput;
+    @FindBy(id = "ff14642ac1c__us_address")
+    private WebElement addressInput;
+    @FindBy(id = "ff14642ac1c__us_phone")
+    private WebElement phoneInput;
+    @FindBy(xpath = ".//input[@value='Save']")
+    private WebElement saveButton;
 
     private WebDriver driver;
     public static final String URL_INSERT = "http://80.92.229.236:81/players/insert";
 
     public CreatePlayerPage(WebDriver driver) {
         this.driver = driver;
+        PageFactory.initElements(driver, this);
     }
 
     public void setUserName(String userName) {
-        WebElement userNameInput = driver.findElement(By.id("ff14642ac1c__us_login"));
         userNameInput.clear();
         userNameInput.sendKeys(userName);
     }
 
     public void setEmail(String email) {
-        WebElement emailInput = driver.findElement(By.id("ff14642ac1c__us_email"));
         emailInput.clear();
         emailInput.sendKeys(email);
     }
 
     public void setPassword(String password) {
-        WebElement passwInput = driver.findElement(By.id("ff14642ac1c__us_password"));
         passwInput.clear();
         passwInput.sendKeys(password);
     }
 
     public void setConfirmPassword(String password) {
-        WebElement confirmPasswordInput = driver.findElement(By.id("ff14642ac1c__confirm_password"));
         confirmPasswordInput.clear();
         confirmPasswordInput.sendKeys(password);
     }
 
     public void setFirstName(String firstName) {
-        WebElement firstNameInput = driver.findElement(By.id("ff14642ac1c__us_fname"));
         firstNameInput.clear();
         firstNameInput.sendKeys(firstName);
     }
 
     public void setLastName(String lastName) {
-        WebElement lastNameInput = driver.findElement(By.id("ff14642ac1c__us_lname"));
         lastNameInput.clear();
         lastNameInput.sendKeys(lastName);
     }
 
     public void setCity(String city) {
-        WebElement cityInput = driver.findElement(By.id("ff14642ac1c__us_city"));
         cityInput.clear();
         cityInput.sendKeys(city);
     }
 
     public void setAddress(String address) {
-        WebElement addressInput = driver.findElement(By.id("ff14642ac1c__us_address"));
         addressInput.clear();
         addressInput.sendKeys(address);
     }
 
     public void setPhone(String phone) {
-        WebElement phoneInput = driver.findElement(By.id("ff14642ac1c__us_phone"));
         phoneInput.clear();
         phoneInput.sendKeys(phone);
     }
 
     public void clickOnSave() {
-        WebElement saveButton = driver.findElement(By.xpath(".//input[@value='Save']"));
         saveButton.click();
     }
 }
