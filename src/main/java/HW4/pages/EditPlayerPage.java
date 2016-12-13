01123065d5e7ba0f27f42b63d1d9b85732210f85
starbass <sereda_sereda1@mail.ru>
2016-12-14 01:18:00 +0200

    ADD:

--- src/main/java/HW4/pages/EditPlayerPage.java
+++ src/main/java/HW4/pages/EditPlayerPage.java
@@ -3,89 +3,106 @@ package HW4.pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class EditPlayerPage {
+    @FindBy(id = "ff14642ac1c__us_email")
+    private WebElement getEmailValue;
+    @FindBy(id = "ff14642ac1c__us_fname")
+    private WebElement getFnameValue;
+    @FindBy(id = "ff14642ac1c__us_lname")
+    private WebElement getLnameValue;
+    @FindBy(id = "ff14642ac1c__us_city")
+    private WebElement getCityValue;
+    @FindBy(id = "ff14642ac1c__us_address")
+    private WebElement getAddressValue;
+    @FindBy(id = "ff14642ac1c__us_phone")
+    private WebElement getPhoneValue;
+    @FindBy(id = "ff14642ac1c__us_email")
+    private WebElement newEmailInput;
+    @FindBy(id = "ff14642ac1c__us_fname")
+    private WebElement newFirstNameInput;
+    @FindBy(id = "ff14642ac1c__us_lname")
+    private WebElement newLastNameInput;
+    @FindBy(id = "ff14642ac1c__us_city")
+    private WebElement newCityInput;
+    @FindBy(id = "ff14642ac1c__us_address")
+    private WebElement newAddressInput;
+    @FindBy(id = "ff14642ac1c__us_phone")
+    private WebElement newPhoneInput;
+    @FindBy(xpath = ".//input[@value='Save']" )
+    private WebElement newSaveButton;
+
 
     private WebDriver driver;
 
     public EditPlayerPage(WebDriver driver) {
         this.driver = driver;
+        PageFactory.initElements(driver, this);
     }
 
     public String getEmail() {
-        WebElement getEmailValue = driver.findElement(By.id("ff14642ac1c__us_email"));
         String emailValue = getEmailValue.getAttribute("value");
         return emailValue;
     }
 
     public String getFirstName() {
-        WebElement getFnameValue = driver.findElement(By.id("ff14642ac1c__us_fname"));
         String fnameValue = getFnameValue.getAttribute("value");
         return fnameValue;
     }
 
     public String getLastName() {
-        WebElement getLnameValue = driver.findElement(By.id("ff14642ac1c__us_lname"));
         String lnameValue = getLnameValue.getAttribute("value");
         return lnameValue;
     }
 
     public String getCity() {
-        WebElement getCityValue = driver.findElement(By.id("ff14642ac1c__us_city"));
         String cityValue = getCityValue.getAttribute("value");
         return cityValue;
     }
 
     public String getAddress() {
-        WebElement getAddressValue = driver.findElement(By.id("ff14642ac1c__us_address"));
         String addressValue = getAddressValue.getText();
         return addressValue;
     }
 
     public String getPhone() {
-        WebElement getPhoneValue = driver.findElement(By.id("ff14642ac1c__us_phone"));
         String phoneValue = getPhoneValue.getAttribute("value");
         return phoneValue;
     }
 
     public void setNewEmail(String newEmail) {
-        WebElement newEmailInput = driver.findElement(By.id("ff14642ac1c__us_email"));
         newEmailInput.clear();
         newEmailInput.sendKeys(newEmail);
     }
 
     public void setNewFirstName(String newFirstName) {
-        WebElement newFirstNameInput = driver.findElement(By.id("ff14642ac1c__us_fname"));
         newFirstNameInput.clear();
         newFirstNameInput.sendKeys(newFirstName);
     }
 
     public void setNewLastName(String newLastName) {
-        WebElement newLastNameInput = driver.findElement(By.id("ff14642ac1c__us_lname"));
         newLastNameInput.clear();
         newLastNameInput.sendKeys(newLastName);
     }
 
     public void setNewCity(String newCity) {
-        WebElement newCityInput = driver.findElement(By.id("ff14642ac1c__us_city"));
         newCityInput.clear();
         newCityInput.sendKeys(newCity);
     }
 
     public void setNewAddress(String newAddress) {
-        WebElement newAddressInput = driver.findElement(By.id("ff14642ac1c__us_address"));
         newAddressInput.clear();
         newAddressInput.sendKeys(newAddress);
     }
 
     public void setNewPhone(String newPhone) {
-        WebElement newPhoneInput = driver.findElement(By.id("ff14642ac1c__us_phone"));
         newPhoneInput.clear();
         newPhoneInput.sendKeys(newPhone);
     }
-
+    
     public void clickNewSaveButton() {
-        WebElement newSaveButton = driver.findElement(By.xpath(".//input[@value='Save']"));
         newSaveButton.click();
     }
 
